57e95c23ea769d53b92548984204811b967db60d
art <pakkawat.kh@gmail.com>
2020-04-29 00:52:49 +0700

    start category...not finish...

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated class="bg-grey-9">
       <q-toolbar>
         <q-btn
           flat
@@ -16,9 +16,15 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
-      Menu
-      <div>Category</div>
-      <div>Product</div>
+      <div class="text-h5 q-py-sm bg-grey-9 text-white" align="center" style="height:50px;">Menu</div>
+      <div class="text-h6 q-py-md q-pl-md">Member</div>
+      <div
+        class="text-h6 q-pb-md q-pl-md"
+        :class="{'bg-grey-8 text-white':pageName=='categoryList'}"
+      >Category</div>
+      <div class="text-h6 q-pb-md q-pl-md">Product</div>
+      <hr />
+      <div class="text-h6 q-pb-md q-pl-md">Log out</div>
     </q-drawer>
     <q-page-container>
       <router-view />
@@ -32,8 +38,22 @@ export default {
 
   data() {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      pageName: "ProductList"
     };
+  },
+  watch: {
+    "$route.name": function(data) {
+      if (data == "categoryList") {
+        this.pageName = "categoryList";
+      } else {
+      }
+    }
+  },
+  mounted() {
+    if (this.$route.name == "categoryList") {
+      this.pageName = "categoryList";
+    }
   }
 };
 </script>
--- src/pages/category.vue
+++ src/pages/category.vue
@@ -1,11 +1,93 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page>
+  <div class="text-body1">
+    <div class="q-pa-md row" style="width:100%; max-width:800px; margin:auto">
+      <div class="col">
+        <table>
+          <tr>
+            <td style="width:80px;">Page :</td>
+            <td>
+              <q-radio v-model="category" val="Furniture" label="Furniture" />
+            </td>
+            <td>
+              <q-radio v-model="category" val="HomeDecor" label="HomeDecor" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="col q-pt-md" align="right">
+        <q-btn label="+ Add New Category" class="bg-grey-8 text-white" no-caps />
+      </div>
+    </div>
+    <div style="width:100%; max-width:800px; margin:auto">
+      <q-table :data="data" :columns="columns" row-key="name" :pagination.sync="pagination">
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="orderId" :props="props" style="width:200px;">
+              <div class="q-py-sm">{{ props.row.orderId }}</div>
+            </q-td>
+            <q-td key="orderId" :props="props">
+              <div class="q-py-sm" align="left">{{ props.row.catName }}</div>
+            </q-td>
+            <q-td key="delete" :props="props" style="width:100px;">
+              <div class="q-py-sm">
+                <u>Delete</u>
+              </div>
+            </q-td>
+            <q-td key="edit" :props="props" style="width:100px;">
+              <div class="q-py-sm">
+                <u>Edit</u>
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+  data() {
+    return {
+      category: "Furniture",
+      pagination: {
+        rowsPerPage: 0
+      },
+      data: [
+        {
+          orderId: 100,
+          catName: "Table",
+          key: "asdfhklkhofsa"
+        },
+        {
+          orderId: 200,
+          catName: "Chair",
+          key: "asdfhkldasdasdkhofsa"
+        }
+      ],
+      columns: [
+        {
+          name: "orderId",
+          label: "Order Id",
+          align: "center"
+        },
+        {
+          name: "catName",
+          label: "Category Name",
+          align: "left"
+        },
+        {
+          name: "delete",
+          label: "Delete",
+          align: "center"
+        },
+        {
+          name: "edit",
+          label: "Edit",
+          align: "center"
+        }
+      ]
+    };
+  }
 };
 </script>
